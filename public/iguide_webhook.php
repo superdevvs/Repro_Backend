43f346e3a31b80bdd6434953c41a48b57eae848f
Shubham Soni <shubhamprasadsony@gmail.com>
2026-05-10 23:03:12 +0530

    fix(iguide): route direct webhook entrypoint to Laravel path

--- public/iguide_webhook.php
+++ public/iguide_webhook.php
@@ -1,3 +1,8 @@
 <?php
 
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__.'/index.php';
+$_SERVER['PHP_SELF'] = '/index.php/iguide_webhook.php';
+$_SERVER['PATH_INFO'] = '/iguide_webhook.php';
+
 require __DIR__.'/index.php';
