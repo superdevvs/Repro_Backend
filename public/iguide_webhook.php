f32c24a79b0d3727364ba190e5d91575db3d846b
Shubham Soni <shubhamprasadsony@gmail.com>
2026-05-10 23:04:09 +0530

    fix(iguide): normalize direct webhook request URI

--- public/iguide_webhook.php
+++ public/iguide_webhook.php
@@ -4,5 +4,6 @@ $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__.'/index.php';
 $_SERVER['PHP_SELF'] = '/index.php/iguide_webhook.php';
 $_SERVER['PATH_INFO'] = '/iguide_webhook.php';
+$_SERVER['REQUEST_URI'] = '/iguide_webhook.php';
 
 require __DIR__.'/index.php';
