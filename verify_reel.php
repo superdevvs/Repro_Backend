2713a17c898dd32781c0c45dd87173ae7304c9aa
Shubham Soni <shubhamprasadsony@gmail.com>
2026-06-10 20:26:23 +0530

    Remove scratch files from repo

--- verify_reel.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo implode(',', \Illuminate\Support\Facades\Schema::getColumnListing('ai_reel_jobs')) . PHP_EOL;
-$m = new \App\Models\AiReelJob();
-echo 'shoot->' . get_class($m->shoot()->getRelated()) . PHP_EOL;
-echo 'user->' . get_class($m->user()->getRelated()) . PHP_EOL;
-echo 'casts: ' . implode(',', array_keys($m->getCasts())) . PHP_EOL;
-echo 'active: ' . var_export((new \App\Models\AiReelJob(['status' => 'processing']))->isActive(), true) . PHP_EOL;
